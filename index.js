2a18ca74c5a6cc398a227fae02f5b043c2e302fb
hgarcia <hernan@dynamicprogrammer.com>
2016-01-05 18:24:53 -0500

    [HG] exposing the checker is a good idea.

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 exports.HealthCheckers = require("./src/health-checkers").HealthCheckers;
 exports.MongoDbHealthChecker = require("./src/mongodb-health-checker").MongoDbHealthChecker;
+exports.SocketHealthChecker = require("./src/socket-health-checker").SocketHealthChecker;
 exports.SQSHealthChecker = require("./src/sqs-health-checker").SQSHealthChecker;
