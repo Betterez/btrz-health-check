0f3ff5d5d8d805434b7d5ebcca2f530e8a6990ae
ezequielleiter <ezequielleiter@gmail.com>
2026-06-16 09:49:54 -0300

    [EL] #86bafbf4e Add support for IMDSv2 ec2 instances metadata

--- src/ec2-metadata.js
+++ src/ec2-metadata.js
@@ -2,15 +2,29 @@ const request = require("superagent");
 
 module.exports = function(field, fn){
   request
-    .get(`http://169.254.169.254/latest/meta-data/${field}`)
+    .put("http://169.254.169.254/latest/api/token")
+    .set("X-aws-ec2-metadata-token-ttl-seconds", "21600")
     .timeout(400)
-    .end(function (err, res) {
+    .end(function(err, res) {
       if (err) {
         return fn(err);
       }
       if (res.status !== 200) {
         return fn(new Error(`${res.status} - ${res.text}`));
       }
-      fn(null, res.text);
+
+      request
+        .get(`http://169.254.169.254/latest/meta-data/${field}`)
+        .set("X-aws-ec2-metadata-token", res.text)
+        .timeout(400)
+        .end(function (err, res) {
+          if (err) {
+            return fn(err);
+          }
+          if (res.status !== 200) {
+            return fn(new Error(`${res.status} - ${res.text}`));
+          }
+          fn(null, res.text);
+        });
     });
 };
